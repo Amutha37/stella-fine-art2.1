bc5a42f47b4d0788132c23de11d7f73248896489
Amutha Muhunthan <tameducate@gmail.com>
2024-03-21 14:15:52 +1100

    removed .default from images object as it no longer suppor

--- src/Assets/Images/Images.js
+++ src/Assets/Images/Images.js
@@ -3,7 +3,7 @@
 //   {
 //     id: 1,
 //     name: 'Peaceful Journey',
-//     image: require('./globalimages/peacefulJourney.jpg').default,
+//     image: require('./globalimages/peacefulJourney.jpg')  ,
 //   },
 // ];
 // Nature
@@ -12,63 +12,63 @@ export const NatureImages = [
       id: 1,
       name: 'Lighthouse in Wollongong',
       size: '40cm(W) x 51cm(H)',
-      image: require('./nature/LighthouseWollongong.jpg').default,
+      image: require('./nature/LighthouseWollongong.jpg')  ,
     },
     {
       id: 2,
       name: 'Wisteria Blooms',
       size: '76cm(W) x 60cm(H)',
-      image: require('./nature/wisteriablooms.jpg').default,
+      image: require('./nature/wisteriablooms.jpg')  ,
     },
     {
       id: 3,
       name: 'Caves beach',
       size: '75cm(W) x 50cm(H)',
-      image: require('./nature/cavesbeach.jpg').default,
+      image: require('./nature/cavesbeach.jpg')  ,
     },
   
     {
       id: 4,
       name: 'Minnamurra Falls',
       size: '23cm(W) x 30cm(H)',
-      image: require('./nature/minnamurrafalls.jpg').default,
+      image: require('./nature/minnamurrafalls.jpg')  ,
     },
     {
       id: 5,
       name: 'Glistening Waters',
       size: '30.5cm(W) x 23cm(H)',
-      image: require('./nature/glisteningwaters.jpg').default,
+      image: require('./nature/glisteningwaters.jpg')  ,
     },
   
     {
       id: 6,
       name: 'Scenic Waters Hawaii',
       size: '30cm(W) x 30cm(H)',
-      image: require('./nature/scenicwatershawaii.jpg').default,
+      image: require('./nature/scenicwatershawaii.jpg')  ,
     },
     {
       id: 7,
       name: 'Shining Gum Tree',
       size: '40cm(W) x 30cm(H)',
-      image: require('./nature/shininggumtree.jpg').default,
+      image: require('./nature/shininggumtree.jpg')  ,
     },
     {
       id: 8,
       name: 'The Lighthouse Wollongong',
       size: '18cm(W) x 13cm(H)',
-      image: require('./nature/thelighthousewollongong.jpeg').default,
+      image: require('./nature/thelighthousewollongong.jpeg')  ,
     },
     {
       id: 9,
       name: 'Serene Waters',
       size: '120cm(W) x 90cm(H)',
-      image: require('./nature/serenewaters.jpg').default,
+      image: require('./nature/serenewaters.jpg')  ,
     },
     {
       id: 10,
       name: 'Beside Still Waters',
       size: '50cm(W) x 40cm(H)',
-      image: require('./nature/besidestillwaters.jpg').default,
+      image: require('./nature/besidestillwaters.jpg')  ,
     },
   ];
   // Floral
@@ -77,45 +77,45 @@ export const NatureImages = [
       id: 1,
       name: 'Mums Hydrangeas In Vase',
       size: '50cm(W) x 50cm(H)',
-      image: require('./floral/hydrangeas.jpg').default,
+      image: require('./floral/hydrangeas.jpg')  ,
     },
     {
       id: 2,
       name: 'Waterlily Study By Mason',
       size: '50cm(W) x 40cm(H)',
-      image: require('./floral/waterlilystudybymason.jpg').default,
+      image: require('./floral/waterlilystudybymason.jpg')  ,
     },
     {
       id: 3,
       name: 'Lifes Sweet Perfume',
       size: '60cm(W) x 30cm(H)',
-      image: require('./floral/lifessweetperfume.jpg').default,
+      image: require('./floral/lifessweetperfume.jpg')  ,
     },
   
     {
       id: 4,
       name: 'Love Blossoms',
       size: '90cm(W) x 76cm(H)',
-      image: require('./floral/loveblossoms.jpg').default,
+      image: require('./floral/loveblossoms.jpg')  ,
     },
     {
       id: 5,
       name: 'Minor bird in Waratah Garden',
       size: '20cm(W) x 25cm(H)',
-      image: require('./floral/minorbirdinwaratahgarden.jpg').default,
+      image: require('./floral/minorbirdinwaratahgarden.jpg')  ,
     },
     {
       id: 6,
       name: 'Skoulariki Fuschia Plant',
       size: '32cm(W) x 27cm(H)',
-      image: require('./floral/skoularikifuschiaplant.jpg').default,
+      image: require('./floral/skoularikifuschiaplant.jpg')  ,
     },
   
     {
       id: 7,
       name: 'Blossomed Tulip',
       size: '50cm(W) x 50cm(H)',
-      image: require('./floral/blossomedtulip.jpg').default,
+      image: require('./floral/blossomedtulip.jpg')  ,
     },
   ];
   
@@ -125,63 +125,63 @@ export const NatureImages = [
       id: 1,
       name: 'Fearsome Warrior',
       size: '50cm(W) x 50cm(H)',
-      image: require('./wildlife/fearsomeWarrior.jpg').default,
+      image: require('./wildlife/fearsomeWarrior.jpg')  ,
     },
     {
       id: 2,
       name: 'Major Mitchell Cockatoo',
       size: '40cm(W) x 50cm(H)',
-      image: require('./wildlife/majorMitchellCockatoo.jpg').default,
+      image: require('./wildlife/majorMitchellCockatoo.jpg')  ,
     },
     {
       id: 3,
       name: 'Blue and Gold Macaw',
       size: '40cm(W) x 50cm(H)',
-      image: require('./wildlife/blueandgoldmacaw.jpg').default,
+      image: require('./wildlife/blueandgoldmacaw.jpg')  ,
     },
     {
       id: 4,
       name: 'Kookaburra Study by Bishal',
       size: '40cm(W) x 50cm(H)',
-      image: require('./wildlife/kookaburrastudybybishal.jpg').default,
+      image: require('./wildlife/kookaburrastudybybishal.jpg')  ,
     },
     {
       id: 5,
       name: 'Lorikeets in Love',
       size: '15cm(W) x 15cm(H)',
-      image: require('./wildlife/lorikeetsinlove.jpg').default,
+      image: require('./wildlife/lorikeetsinlove.jpg')  ,
     },
   
     {
       id: 6,
       name: 'Sulfar Crested Cockatoo in Fruit Tree',
       size: '76cm(W) x 51cm(H)x3.5cm(D)',
-      image: require('./wildlife/sulfarcrestedcockatooinfruittree.jpg').default,
+      image: require('./wildlife/sulfarcrestedcockatooinfruittree.jpg')  ,
     },
     {
       id: 7,
       name: 'Green Parrot in Tree',
       size: '31cm(W) x 39cm(H)',
-      image: require('./wildlife/greenparrotintree.jpg').default,
+      image: require('./wildlife/greenparrotintree.jpg')  ,
     },
   
     {
       id: 8,
       name: 'Silver-Gull by the Beach',
       size: '30cm(W) x 30cm(H)',
-      image: require('./wildlife/silvergullbythebeach.jpg').default,
+      image: require('./wildlife/silvergullbythebeach.jpg')  ,
     },
     {
       id: 9,
       name: 'Majesty',
       size: '31cm(W) x 41cm(H)',
-      image: require('./wildlife/majesty.jpg').default,
+      image: require('./wildlife/majesty.jpg')  ,
     },
     {
       id: 10,
       name: 'Baby Blue Sea Turtle',
       size: '30cm(W) x 30cm(H)',
-      image: require('./wildlife/babyblueseaturtle.jpg').default,
+      image: require('./wildlife/babyblueseaturtle.jpg')  ,
     },
   ];
   
@@ -191,34 +191,34 @@ export const NatureImages = [
       id: 1,
       name: 'Young woman by a window - Berthe Moriset',
       size: '',
-      image: require('./mastercopies/youngwomanbyawindow.jpg').default,
+      image: require('./mastercopies/youngwomanbyawindow.jpg')  ,
     },
   
     {
       id: 2,
       name: 'Play in the Surf - Edward Potthast',
       size: '',
-      image: require('./mastercopies/playinthesurf.jpg').default,
+      image: require('./mastercopies/playinthesurf.jpg')  ,
     },
   
     {
       id: 3,
       name: 'The Grand Canal of Venice - Edouard Manet',
       size: '',
-      image: require('./mastercopies/thegrandcanalofvenice.jpg').default,
+      image: require('./mastercopies/thegrandcanalofvenice.jpg')  ,
     },
     {
       id: 4,
       name: 'Girl in Pink Dress - Michael Peter Ancher',
       size: '',
-      image: require('./mastercopies/girlinpinkdress.jpg').default,
+      image: require('./mastercopies/girlinpinkdress.jpg')  ,
     },
     {
       id: 5,
       name: 'Rembrandt Study',
       // size: "31cm(W)x41cm(H)",
       size: '',
-      image: require('./mastercopies/rembrandtstudy.jpg').default,
+      image: require('./mastercopies/rembrandtstudy.jpg')  ,
     },
   ];
   
@@ -228,32 +228,32 @@ export const NatureImages = [
       id: 1,
       name: 'Martin Luther King',
       size: '',
-      image: require('./portrait/martinlutherking.jpg').default,
+      image: require('./portrait/martinlutherking.jpg')  ,
     },
     {
       id: 2,
       name: 'Portrait 1',
       size: '',
-      image: require('./portrait/Effie.jpg').default,
+      image: require('./portrait/Effie.jpg')  ,
     },
     {
       id: 3,
       name: 'Portrait 2',
       size: '',
-      image: require('./portrait/portrait1.jpg').default,
+      image: require('./portrait/portrait1.jpg')  ,
     },
   
     {
       id: 4,
       name: 'Portrait 2',
       size: '',
-      image: require('./portrait/portrait2.jpg').default,
+      image: require('./portrait/portrait2.jpg')  ,
     },
     {
       id: 5,
       name: 'Christopher',
       size: '',
-      image: require('./portrait/christopher.jpg').default,
+      image: require('./portrait/christopher.jpg')  ,
     },
   ];
   
@@ -263,25 +263,25 @@ export const NatureImages = [
       id: 1,
       name: 'Princess the Cat',
       size: '',
-      image: require('./petportraits/princessthecat.jpg').default,
+      image: require('./petportraits/princessthecat.jpg')  ,
     },
     {
       id: 2,
       name: 'Striking Macaws',
       size: '',
-      image: require('./petportraits/strikingmacaws.jpg').default,
+      image: require('./petportraits/strikingmacaws.jpg')  ,
     },
     {
       id: 3,
       name: 'Charlie',
       size: '',
-      image: require('./petportraits/charlie.jpg').default,
+      image: require('./petportraits/charlie.jpg')  ,
     },
     {
       id: 4,
       name: 'Lorikeet',
       size: '',
-      image: require('./petportraits/lorikeet.jpg').default,
+      image: require('./petportraits/lorikeet.jpg')  ,
     },
   ];
   // sold
@@ -290,67 +290,104 @@ export const NatureImages = [
       id: 1,
       name: "Nature's Beauty",
       size: '40cm(W) x 50cm(H)',
-      image: require('./sold/naturebeauty.jpg').default,
+      image: require('./sold/naturebeauty.jpg')  ,
     },
     {
       id: 2,
       name: 'Deer in Wilderness',
       size: '25cm(W) x 30cm(H)',
-      image: require('./sold/deerinwilderness.jpg').default,
+      image: require('./sold/deerinwilderness.jpg')  ,
     },
     {
       id: 3,
       name: 'Hang-in-there Koala Bear',
       size: '  20cm(W) x 25cm(H) ',
-      image: require('./sold/hangintherekoalabear.jpg').default,
+      image: require('./sold/hangintherekoalabear.jpg')  ,
     },
     {
       id: 4,
       name: 'Aussie Lorikeet',
       size: '25cm(W) x 30cm(H) ',
-      image: require('./sold/aussielorikeet.jpg').default,
+      image: require('./sold/aussielorikeet.jpg')  ,
     },
     {
       id: 5,
       name: 'Calming Florals(Commission Piece)',
       size: '76cm(W) x 76cm(H)  ',
-      image: require('./sold/calmingflorals.jpg').default,
+      image: require('./sold/calmingflorals.jpg')  ,
     },
     {
       id: 6,
       name: 'Budgies in Love',
       size: '30cm(W) x 30cm(H)  ',
-      image: require('./sold/budgiesinlove.jpg').default,
+      image: require('./sold/budgiesinlove.jpg')  ,
     },
     {
       id: 7,
       name: 'Deep Blue Sea Turtle',
       size: '45cm(W) x 60cm(H)  ',
-      image: require('./sold/deepblueseaturtle.jpg').default,
+      image: require('./sold/deepblueseaturtle.jpg')  ,
     },
     {
       id: 8,
       name: 'Peaceful Journey(Commission Piece)',
       size: '90cm(W) x 120cm(H)  ',
-      image: require('./sold/peacefuljourney.jpg').default,
+      image: require('./sold/peacefuljourney.jpg')  ,
     },
     {
       id: 9,
       name: 'On The Road to Minnamurra Falls',
       size: '11cm(W) x 18cm(H)  ',
-      image: require('./sold/ontheroadtominnamurrafalls.jpg').default,
+      image: require('./sold/ontheroadtominnamurrafalls.jpg')  ,
     },
   ];
   
   // commission images sample display
   
+  // export const CommissionSamples = [
+  //   {
+  //     id: 1,
+  //     name: 'Calming Florals 2020',
+  //     //  name: 'Calming Florals 2020 76cm x 76cm x 4cm depth Oil on stretched canvas',
+  //     size: '76cm x 76cm x 4cm depth',
+  //     info: 'Oil on stretched canvas',
+  //     image: require(`./sold/calmingflorals.jpg`)  ,
+  //     clName: 'cactive',
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Martin Luther King',
+  //     size: '',
+  //     info: '',
+  //     image: require('./portrait/martinlutherking.jpg')  ,
+  //     clName: 'cactive',
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Peaceful Journey 2021',
+  //     size: '120cm x 90cm x 4cm depth',
+  //     info: 'Oil on stretched canvas',
+  //     image: require('./sold/peacefuljourney.jpg')  ,
+  //     clName: 'cactive',
+  //   },
+  //   {
+  //     id: 4,
+  //     name: 'Charlie',
+  //     size: '',
+  //     info: '',
+  //     image: require('./petportraits/charlie.jpg')  ,
+  //     clName: 'cactive',
+  //   },
+  // ];
+
   export const CommissionSamples = [
     {
       id: 1,
-      name: 'Calming Florals 2020 76cm x 76cm x 4cm depth Oil on stretched canvas',
+      name: 'Calming Florals 2020',
+      //  name: 'Calming Florals 2020 76cm x 76cm x 4cm depth Oil on stretched canvas',
       size: '76cm x 76cm x 4cm depth',
       info: 'Oil on stretched canvas',
-      image: require('./sold/calmingflorals.jpg').default,
+      image: require(`./sold/calmingflorals.jpg`),
       clName: 'cactive',
     },
     {
@@ -358,7 +395,7 @@ export const NatureImages = [
       name: 'Martin Luther King',
       size: '',
       info: '',
-      image: require('./portrait/martinlutherking.jpg').default,
+      image: require('./portrait/martinlutherking.jpg'),
       clName: 'cactive',
     },
     {
@@ -366,7 +403,7 @@ export const NatureImages = [
       name: 'Peaceful Journey 2021',
       size: '120cm x 90cm x 4cm depth',
       info: 'Oil on stretched canvas',
-      image: require('./sold/peacefuljourney.jpg').default,
+      image: require('./sold/peacefuljourney.jpg'),
       clName: 'cactive',
     },
     {
@@ -374,7 +411,7 @@ export const NatureImages = [
       name: 'Charlie',
       size: '',
       info: '',
-      image: require('./petportraits/charlie.jpg').default,
+      image: require('./petportraits/charlie.jpg'),
       clName: 'cactive',
     },
   ];
@@ -385,13 +422,13 @@ export const NatureImages = [
       id: 1,
       name: 'Great Ocean Road',
       size: 'none',
-      image: require('./morepaintings/greatoceanroad.jpg').default,
+      image: require('./morepaintings/greatoceanroad.jpg'),
     },
     {
       id: 2,
       name: 'The Three Sisters',
       size: 'none',
-      image: require('./morepaintings/threesisters.jpg').default,
+      image: require('./morepaintings/threesisters.jpg'),
     },
   ];
   
